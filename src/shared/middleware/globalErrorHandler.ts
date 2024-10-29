0f11c4f004bbd26581c94c503b44e8944fe0dd03
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2024-10-29 16:38:32 -0300

    feat: globalErrorHandler

--- src/shared/middleware/globalErrorHandler.ts
+++ src/shared/middleware/globalErrorHandler.ts
@@ -1,45 +1,34 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@shared/error/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-import { AppError } from '../error/AppError';
+interface IZodError {
+  name: string;
+  errors: Array<{ message: string }>;
+}
 
 export async function globalErrorHandler(
-  err: Error,
-  request: Request,
-  response: Response,
-  _: NextFunction,
-): Promise<Response<any>> {
-  const ip = request.headers['x-forwarded-for'] || request.ip;
-  if (err instanceof AppError) {
-    // logger.error(
-    //   `${err.statusCode} - ${err.message} - ${request.originalUrl} - ${
-    //     request.method
-    //   } - ${ip} - body: ${JSON.stringify(
-    //     request.body,
-    //   )} - params: ${JSON.stringify(request.params)} - query: ${JSON.stringify(
-    //     request.query,
-    //   )} - user: ${'teset'} - date: ${new Date()}`,
-    // );
-
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
+  error: Error | AppError | IZodError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): Promise<any> {
+  if ((error as IZodError)?.name === 'ZodError') {
+    return res.status(400).json({
+      message: `${(error as ZodError)?.errors[0]?.message}`,
+      statusCode: 400,
     });
   }
 
-  // logger.error(
-  //   `${500} - ${err.message} - ${request.originalUrl} - ${request.method} - ${
-  //     ip
-  //   } - body: ${JSON.stringify(request.body)} - params: ${JSON.stringify(
-  //     request.params,
-  //   )} - query: ${JSON.stringify(request.query)} - user: ${JSON.stringify(
-  //     request.user,
-  //   )} - date: ${new Date()}`,
-  // );
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ message: error.message, statusCode: error.statusCode });
+  }
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Server error',
-  });
+  console.error(error);
+
+  return res
+    .status(500)
+    .json({ message: 'Internal server error', statusCode: 500 });
 }
