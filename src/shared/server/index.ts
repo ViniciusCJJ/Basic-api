1026187e6409abb353c769b07cc0717f84adbbcc
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2024-11-11 13:50:20 -0300

    feat: server start url/yarn lock

--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -2,12 +2,16 @@ import 'dotenv/config';
 import 'express-async-errors';
 import 'reflect-metadata';
 import '@shared/database';
-
+import 'src/shared/providers/RedisProvider/index';
 import { app } from '@shared/server/app';
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT} , URL: ${
+      process.env.BASE_URL || 'http://localhost:3333'
+    }`,
+  );
 });
