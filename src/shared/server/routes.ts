6ff5fcc85ec37753b2687e93cda930628f2fcdd2
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2024-11-11 13:48:57 -0300

    feat: add swagger

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -1,7 +1,21 @@
 import { userRouter } from '@modules/User/routes/user.routes';
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '@config/swagger.json';
 
 const router = Router();
+
+router.get('/', (_req: Request, res: Response) => {
+  res.send(
+    `${process.env.API_NAME || 'BasicBoilerplate'} ${
+      process.env.API_VERSION || 'v1.0.0'
+    }`,
+  );
+});
+
+router.use('/api-docs', swaggerUi.serve);
+
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.use('/user', userRouter);
 
