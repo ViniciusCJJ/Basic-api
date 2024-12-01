dbc9c14e0afd036da6ae1d83ff32a1b027389c55
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2024-12-01 19:14:46 -0300

    fix: use shortcut

--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import 'express-async-errors';
 import 'reflect-metadata';
 import '@shared/database';
-import 'src/shared/providers/RedisProvider/index';
+import '@shared/providers/RedisProvider/index';
 import { app } from '@shared/server/app';
 
 const PORT = process.env.PORT || 3333;
