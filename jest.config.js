853dcdd09147713ca64cf4bd515f1f6b3b974508
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2024-12-02 13:29:27 -0300

    fix: test only js(x) files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: [
-    '**/__tests__/**/*.test.js?(x)',
-    '**/?(*.)+(spec|test).js?(x)',
-    '**/__tests__/**/*.test.ts?(x)',
-    '**/?(*.)+(spec|test).ts?(x)',
-  ],
+  testMatch: ['**/__tests__/**/*.test.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
 };
