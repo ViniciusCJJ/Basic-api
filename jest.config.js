551bba15e32655f4fa78642df4963c3531043a51
ViniciusCJJ <85507924+ViniciusCJJ@users.noreply.github.com>
2026-01-04 14:49:46 -0300

    chore: add ts-jest and shortcuts

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,20 @@
 module.exports = {
   testEnvironment: 'node',
+  preset: 'ts-jest',
   testMatch: [
     // '**/__tests__/**/*.test.js?(x)',
     // '**/?(*.)+(spec|test).js?(x)',
     '**/__tests__/**/*.test.ts?(x)',
     '**/?(*.)+(spec|test).ts?(x)',
   ],
+  moduleNameMapper: {
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@error/(.*)$': '<rootDir>/src/shared/errors/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@prisma/client$': '<rootDir>/src/shared/database/generated/prisma/client',
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 };
